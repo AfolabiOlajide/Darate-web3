556f1ebf9b1179a6d0de0991314abc92d01d7aed
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-03-04 12:23:25 +0100

    initial configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    networks: {
+      hardhat: {},
+      fantom_testnet: {
+        rpc: "https://rpc.ankr.com/fantom_testnet	",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+      fantom: {
+        rpc: "",
+        accounts: [`0x${process.env.PRIVATE_KEY}`], 
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
